5943b0106da9217dc64f0e9cf5e3e8d41eb678d9
deprak <dedprakasa@gmail.com>
2019-11-20 13:26:17 +0700

    cek

--- models/categorymovies.js
+++ models/categorymovies.js
@@ -10,7 +10,6 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER
     },
     CategoryId: DataTypes.INTEGER,
-    VoterId: DataTypes.INTEGER,
     MovieId: DataTypes.INTEGER
   }, {sequelize, modelName: 'CategoryMovies'});
   CategoryMovies.associate = function(models) {
